fc73db14bf1ac6be623720383757cbbd4616067a
ZaStack <stackz.personal@gmail.com>
2020-02-21 12:06:05 -0800

    MVP

--- smurfs/src/components/smurfForm.js
+++ smurfs/src/components/smurfForm.js
@@ -1,35 +1,44 @@
 import React, { useContext } from 'react';
 import { FormContext } from '../contexts/formContext';
+import styled from 'styled-components';
+
+const FormDiv = styled.div `
+    border: 3px solid navy;
+    margin: 5%;
+`
 
 
 const SmurfForm = () => {
     const { handleChanges, handleSubmit, smurf} = useContext(FormContext)
 
     return (
-        <form onSubmit={ handleSubmit }>
-            <label htmlFor='name'>Name</label>
-            <input type="text"
-                    id='name'
-                    name='name'
-                    onChange={ handleChanges }
-                    value={ smurf.name }
-            />
-            <label htmlFor="age">Age</label>
-            <input type="text"
-                    id='age'
-                    name='age'
-                    onChange={ handleChanges }
-                    value={ smurf.age }
-            />
-            <label htmlFor="height">Height</label>
-            <input type="text"
-                    id='height'
-                    name='height'
-                    onChange={ handleChanges }
-                    value={ smurf.height }
-            />
-            <button type='submit' onClick={handleSubmit}>Add Smurf</button>
+        <FormDiv>
+            <form onSubmit={ handleSubmit }>
+                <label htmlFor='name'>Name</label>
+                <input type="text"
+                        id='name'
+                        name='name'
+                        onChange={ handleChanges }
+                        value={ smurf.name }
+                />
+                <label htmlFor="age">Age</label>
+                <input type="text"
+                        id='age'
+                        name='age'
+                        onChange={ handleChanges }
+                        value={ smurf.age }
+                />
+                <label htmlFor="height">Height</label>
+                <input type="text"
+                        id='height'
+                        name='height'
+                        onChange={ handleChanges }
+                        value={ smurf.height }
+                />
+                <button type='submit' onClick={handleSubmit}>Add Smurf</button>
         </form>
+        </FormDiv>
+
     )
 }
 
--- smurfs/src/components/smurfList.js
+++ smurfs/src/components/smurfList.js
@@ -1,6 +1,20 @@
 import React, { useContext } from "react";
 import { SmurfContext } from "../contexts/smurfContext";
 import cuid from "cuid";
+import styled from 'styled-components';
+
+const SmurfCard = styled.div`
+    background-color: navy;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    width: 40%
+    padding: 5%;
+    
+`
 
 const SmurfList = () => {
     const { smurfList } = useContext(SmurfContext);
@@ -9,11 +23,11 @@ const SmurfList = () => {
     return (
         <div className="smurf-list">
             {smurfList.map(smurf => (
-            <div className="smurf" key={cuid()}>
+            <SmurfCard key={cuid()}>
                 <h2>Name:{smurf.name}</h2>
                 <p>Age:{smurf.age}</p>
                 <p>Height:{smurf.height}</p>
-            </div>
+            </SmurfCard>
             ))}
         </div>
     )}
